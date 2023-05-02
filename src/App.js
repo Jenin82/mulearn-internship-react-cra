c7686eae4dde44589a1f87c2b0c9999d26773f7f
Jenin82 <jenin8282@gmail.com>
2023-05-02 21:54:18 +0530

    made card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/card.component';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Mulearn project done so far:</h1>
+			<hr></hr>
+			<Card title={"Django Todo Backend"} description={"Build a django project in which users can signup/login and create TODOs."}/>
+			<hr></hr>
+			<Card title={"Django Todo REST API"} description={"Build APIs for all the features of the TODO app and tested it with postman"}/>
+			<hr></hr>
+			<Card title={"React App CRA"} description={"Made a card component to display what you see right now"}/>
+			<hr></hr>
     </div>
   );
 }
